e015403cb74ac7fefd88f4202fe1cf7451288c44
Rion <info@ri0n.dev>
2025-08-17 03:19:41 +0900

    add: vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-
+import { Analytics } from "@vercel/analytics/next"
 import Header from "@/components/header";
 import Footer from "@/components/layout/footer";
 import "./globals.css";
@@ -49,6 +49,9 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <Header />
         {children}
         <Footer />
+
+        {/* Vercel Analytics */}
+        <Analytics />
       </body>
     </html>
   );
